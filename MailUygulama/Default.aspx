0805dbf84eda04f3726196d41f79d86b5122f2db
nursahdagli <nursahdagli8@gmail.com>
2019-10-02 14:54:50 +0300

    Mail Uygulama

---
+++ MailUygulama/Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MailUygulama.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <center>
+    <table>
+    <form id="form1" runat="server">
+        <tr>
+            <td>Ad Soyad:</td>
+            <td><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox></td>    
+        </tr>
+        <tr>
+            <td>Email:</td>      
+            <td><asp:TextBox ID="TextBox2" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>Konu:</td>      
+            <td><asp:TextBox ID="TextBox3" runat="server"></asp:TextBox> </td>
+        </tr>
+         <tr>
+            <td>Mesaj:</td>      
+            <td><asp:TextBox ID="TextBox4" runat="server" Height="140px" Width="188px" TextMode="MultiLine"></asp:TextBox> </td>
+        </tr>
+        <tr>
+        <td>&nbsp;</td>
+        <td>
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+        </td>
+        </tr>
+        <tr>
+        <td>&nbsp;</td>
+        <td>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Gönder" />
+        </td>
+        </tr>
+    </form>
+    </table>
+        </center>
+</body>
+</html>
---
+++ MailUygulama/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MailUygulama
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MailGonder();
+        }
+
+        public void MailGonder()
+        {
+            System.Net.Mail.MailMessage msg = new System.Net.Mail.MailMessage();
+            msg.To.Add("nursahdagli8@gmail.com"); // epostanın kime gönderilecegi bilgisini tutar.
+            msg.From = new System.Net.Mail.MailAddress(TextBox2.Text, "Mail Baslik", System.Text.Encoding.UTF8);  // epostanın kimden gönderilecegi bilgisini tutar.
+            msg.Subject = TextBox3.Text; // epostanın konu bilgisini tutar.
+            msg.Body = TextBox4.Text; //epostanın icerik bilgisini tutar.
+            System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient();// E-Posta'nin gönderilecegi SMTP sunucu ve gönderen kullanicinin bilgilerinin yazilip, MailMessage türünde olusturulan mailin gönderildigi siniftir.
+            client.Credentials = new System.Net.NetworkCredential("nursahdagli8@gmail.com", "TsMnA5862+");// epostayı gönderen kullanıcının bilgileri
+            client.Port = 587; //veya 25
+            client.Host = "smtp.gmail.com"; //smtp sunucusunun isim bilgisini tutar
+            client.EnableSsl = true; // sunucu ssl istiyorsa bunu true yapıcaz
+            client.Send(msg);
+            Label1.Text = "Mailiniz Başarı İle Gönderildi";
+        }
+    }
+}
